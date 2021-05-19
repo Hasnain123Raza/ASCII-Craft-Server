0e43e650aa93086f67cdec95fb5b37733b25a701
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-05-19 17:33:04 -0400

    Added /open/art/:artId and removed sending server errors over in responses.

--- routers/api/art/open/index.js
+++ routers/api/art/open/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import artModel from "../../../../services/database/models/art.js";
 
 const router = express.Router();
+
+router.get("/art/:artId", async (request, response) => {
+  const { artId } = request.params;
+
+  try {
+    const art = await artModel.findById(artId);
+
+    response.status(200).json({ success: true, payload: art });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ success: false });
+  }
+});
 
 export default router;
