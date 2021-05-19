5913b2357dd4173f4748804a1cc5edd411c268d5
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-05-19 12:04:27 -0400

    Renamed /simplifiedArt to /simplifiedArts

--- routers/api/art/browse/index.js
+++ routers/api/art/browse/index.js
@@ -12,37 +12,40 @@ router.get("/artCount", async (request, response) => {
   }
 });
 
-router.get("/simplifiedArt/:pageIndex/:pageSize", async (request, response) => {
-  const params = request.params;
-  const pageIndex = parseInt(params.pageIndex);
-  const pageSize = parseInt(params.pageSize);
+router.get(
+  "/simplifiedArts/:pageIndex/:pageSize",
+  async (request, response) => {
+    const params = request.params;
+    const pageIndex = parseInt(params.pageIndex);
+    const pageSize = parseInt(params.pageSize);
 
-  if (
-    isNaN(pageIndex) ||
-    isNaN(pageSize) ||
-    pageIndex < 0 ||
-    pageSize < 1 ||
-    pageSize > 12
-  ) {
-    return response.status(400).json({ success: false });
-  } else {
-    try {
-      const arts = await artModel
-        .find({})
-        .skip(pageIndex * pageSize)
-        .limit(pageSize);
-      const simplifiedArts = arts.map(({ _id, title, description }) => ({
-        _id,
-        title,
-        description,
-      }));
+    if (
+      isNaN(pageIndex) ||
+      isNaN(pageSize) ||
+      pageIndex < 0 ||
+      pageSize < 1 ||
+      pageSize > 12
+    ) {
+      return response.status(400).json({ success: false });
+    } else {
+      try {
+        const arts = await artModel
+          .find({})
+          .skip(pageIndex * pageSize)
+          .limit(pageSize);
+        const simplifiedArts = arts.map(({ _id, title, description }) => ({
+          _id,
+          title,
+          description,
+        }));
 
-      response.status(200).json({ success: true, payload: simplifiedArts });
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({ success: false });
+        response.status(200).json({ success: true, payload: simplifiedArts });
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({ success: false });
+      }
     }
   }
-});
+);
 
 export default router;
