31a7f3df4e3583c4db1b69e16531dbc023daad5b
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-05-31 10:20:33 -0400

    Added messages for Joi validation schemas.

--- routers/api/art/create/artSchema.js
+++ routers/api/art/create/artSchema.js
@@ -1,7 +1,20 @@
 import Joi from "joi";
 
 export default Joi.object({
-  title: Joi.string().trim().min(1).max(30).required(),
-  description: Joi.string().trim().min(1).max(100).required(),
-  content: Joi.string().min(1).max(4000).required(),
+  title: Joi.string().trim().min(1).max(30).required().messages({
+    "string.empty": "Title is required.",
+    "string.min": "Title must be at least 1 characters long.",
+    "string.max": "Title must be less than or equal to 30 characters long.",
+  }),
+  description: Joi.string().trim().min(1).max(100).required().messages({
+    "string.empty": "Description is required.",
+    "string.min": "Description must be at least 1 characters long.",
+    "string.max":
+      "Description must be less than or equal to 100 characters long.",
+  }),
+  content: Joi.string().min(1).max(4000).required().messages({
+    "string.empty": "Content is required.",
+    "string.min": "Content must be at least 1 characters long.",
+    "string.max": "Content must be less than or equal to 4000 characters long.",
+  }),
 });
--- routers/api/authentication/userSchema.js
+++ routers/api/authentication/userSchema.js
@@ -1,11 +1,21 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
-  username: Joi.string().alphanum().trim().min(3).max(20).required(),
-  password: Joi.string().min(10).max(100).required(),
+  username: Joi.string().alphanum().trim().min(3).max(20).required().messages({
+    "string.empty": "Username is required.",
+    "string.min": "Username must be at least 3 characters long.",
+    "string.max": "Username must be less than or equal to 20 characters long.",
+  }),
+  password: Joi.string().min(10).max(100).required().messages({
+    "string.empty": "Password is required.",
+    "string.min": "Password must be at least 10 characters long.",
+    "string.max": "Password must be less than or equal to 100 characters long.",
+  }),
 });
 
 export default Joi.object({
   user: userSchema,
-  recaptchaToken: Joi.string().required(),
+  recaptchaToken: Joi.string().required().messages({
+    "string.empty": "reCaptcha is required.",
+  }),
 });
