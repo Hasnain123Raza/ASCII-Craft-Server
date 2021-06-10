9339857ffb7abf0866392167f9054fe264819c24
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-06-10 16:35:23 -0400

    Updated Profile and Browse to allow browsing liked arts of other people.

--- routers/api/art/browse/index.js
+++ routers/api/art/browse/index.js
@@ -5,14 +5,34 @@ import mongodb from "mongodb";
 
 const router = express.Router();
 
-router.get("/artCount", async (request, response) => {
-  const { userId } = request.query;
-  if (Boolean(userId) && !validateUserId(userId))
+router.post("/artCount", async (request, response) => {
+  // const { userId } = request.query;
+  // if (Boolean(userId) && !validateUserId(userId))
+  //   response.status(400).json({ success: false });
+
+  // try {
+  //   const count = Boolean(userId)
+  //     ? await getUserArtCount(new mongodb.ObjectId(userId))
+  //     : await getAllArtCount();
+
+  //   if (!Boolean(count) && count !== 0)
+  //     return response.status(500).json({ success: false });
+
+  //   return response.status(200).json({ success: true, payload: count });
+  // } catch (error) {
+  //   console.log(error);
+  //   return response.status(500).json({ success: false });
+  // }
+
+  const { selectors } = request.body;
+  const selector = selectors.find((element) => element.payload !== null);
+
+  if (Boolean(selector) && !validateUserId(selector.payload))
     response.status(400).json({ success: false });
 
   try {
-    const count = Boolean(userId)
-      ? await getUserArtCount(new mongodb.ObjectId(userId))
+    const count = Boolean(selector)
+      ? await getUserArtCount(selector)
       : await getAllArtCount();
 
     if (!Boolean(count) && count !== 0)
@@ -25,9 +45,38 @@ router.get("/artCount", async (request, response) => {
   }
 });
 
-router.get(
+router.post(
   "/simplifiedArts/:pageIndex/:pageSize",
   async (request, response) => {
+    //   const params = request.params;
+    //   const pageIndex = parseInt(params.pageIndex);
+    //   const pageSize = parseInt(params.pageSize);
+
+    //   if (!validatePageIndexAndPageSize(pageIndex, pageSize))
+    //     return response.status(400).json({ success: false });
+
+    //   const { userId } = request.query;
+    //   if (Boolean(userId) && !validateUserId(userId))
+    //     return response.status(400).json({ success: false });
+
+    //   try {
+    //     const simplifiedArts = Boolean(userId)
+    //       ? await getUserSimplifiedArts(
+    //           new mongodb.ObjectId(userId),
+    //           pageIndex,
+    //           pageSize
+    //         )
+    //       : await getAllSimplifiedArts(pageIndex, pageSize);
+
+    //     return response
+    //       .status(200)
+    //       .json({ success: true, payload: simplifiedArts });
+    //   } catch (error) {
+    //     console.log(error);
+    //     return response.status(500).json({ success: false });
+    //   }
+    // }
+
     const params = request.params;
     const pageIndex = parseInt(params.pageIndex);
     const pageSize = parseInt(params.pageSize);
@@ -35,17 +84,12 @@ router.get(
     if (!validatePageIndexAndPageSize(pageIndex, pageSize))
       return response.status(400).json({ success: false });
 
-    const { userId } = request.query;
-    if (Boolean(userId) && !validateUserId(userId))
-      return response.status(400).json({ success: false });
+    const { selectors } = request.body;
+    const selector = selectors.find((element) => element.payload !== null);
 
     try {
-      const simplifiedArts = Boolean(userId)
-        ? await getUserSimplifiedArts(
-            new mongodb.ObjectId(userId),
-            pageIndex,
-            pageSize
-          )
+      const simplifiedArts = Boolean(selector)
+        ? await getUserSimplifiedArts(selector, pageIndex, pageSize)
         : await getAllSimplifiedArts(pageIndex, pageSize);
 
       return response
--- routers/api/art/browse/userArts.js
+++ routers/api/art/browse/userArts.js
@@ -1,50 +1,111 @@
 import artModel from "../../../../services/database/models/art.js";
 import userModel from "../../../../services/database/models/user.js";
+import mongodb from "mongodb";
 
-export async function getUserArtCount(userId) {
-  const userAggregate = await userModel
-    .aggregate([
-      { $match: { _id: userId } },
-      { $project: { totalArtsCreated: { $size: "$createdArtIds" } } },
-    ])
-    .exec();
+export async function getUserArtCount({ type, payload }) {
+  payload = new mongodb.ObjectId(payload);
 
-  return userAggregate[0]?.totalArtsCreated;
+  if (type === "createdByUserId") {
+    const userAggregate = await userModel
+      .aggregate([
+        { $match: { _id: payload } },
+        { $project: { totalArtsCreated: { $size: "$createdArtIds" } } },
+      ])
+      .exec();
+
+    return userAggregate[0]?.totalArtsCreated;
+  } else if (type === "likedByUserId") {
+    const userAggregate = await userModel
+      .aggregate([
+        { $match: { _id: payload } },
+        { $project: { totalArtsLiked: { $size: "$likedArtIds" } } },
+      ])
+      .exec();
+
+    return userAggregate[0]?.totalArtsLiked;
+  }
 }
 
-export async function getUserSimplifiedArts(userId, pageIndex, pageSize) {
-  const userAggregate = await userModel
-    .aggregate([
-      { $match: { _id: userId } },
-      {
-        $project: {
-          _id: 1,
-          createdArtIds: { $reverseArray: "$createdArtIds" },
+export async function getUserSimplifiedArts(
+  { type, payload },
+  pageIndex,
+  pageSize
+) {
+  payload = new mongodb.ObjectId(payload);
+
+  if (type === "createdByUserId") {
+    const userAggregate = await userModel
+      .aggregate([
+        { $match: { _id: payload } },
+        {
+          $project: {
+            _id: 1,
+            createdArtIds: { $reverseArray: "$createdArtIds" },
+          },
         },
-      },
-      { $unwind: { path: "$createdArtIds", preserveNullAndEmptyArrays: true } },
-      { $skip: pageIndex * pageSize },
-      { $limit: pageSize },
-      {
-        $group: {
-          _id: "$_id",
-          createdArtIds: { $push: "$createdArtIds" },
+        {
+          $unwind: { path: "$createdArtIds", preserveNullAndEmptyArrays: true },
         },
-      },
-    ])
-    .exec();
+        { $skip: pageIndex * pageSize },
+        { $limit: pageSize },
+        {
+          $group: {
+            _id: "$_id",
+            createdArtIds: { $push: "$createdArtIds" },
+          },
+        },
+      ])
+      .exec();
 
-  if (userAggregate.length === 0) return [];
+    if (userAggregate.length === 0) return [];
 
-  const userData = userAggregate[0];
-  const userArtIds = userData.createdArtIds;
+    const userData = userAggregate[0];
+    const userArtIds = userData.createdArtIds;
 
-  const simplifiedArts = await artModel
-    .aggregate([
-      { $match: { _id: { $in: userArtIds } } },
-      { $project: { _id: 1, title: 1, description: 1 } },
-    ])
-    .exec();
+    const simplifiedArts = await artModel
+      .aggregate([
+        { $match: { _id: { $in: userArtIds } } },
+        { $project: { _id: 1, title: 1, description: 1 } },
+      ])
+      .exec();
 
-  return simplifiedArts.reverse();
+    return simplifiedArts;
+  } else if (type === "likedByUserId") {
+    const userAggregate = await userModel
+      .aggregate([
+        { $match: { _id: payload } },
+        {
+          $project: {
+            _id: 1,
+            likedArtIds: { $reverseArray: "$likedArtIds" },
+          },
+        },
+        {
+          $unwind: { path: "$likedArtIds", preserveNullAndEmptyArrays: true },
+        },
+        { $skip: pageIndex * pageSize },
+        { $limit: pageSize },
+        {
+          $group: {
+            _id: "$_id",
+            likedArtIds: { $push: "$likedArtIds" },
+          },
+        },
+      ])
+      .exec();
+
+    if (userAggregate.length === 0) return [];
+
+    const userData = userAggregate[0];
+    const userArtIds = userData.likedArtIds;
+
+    const simplifiedArts = await artModel
+      .aggregate([
+        { $match: { _id: { $in: userArtIds } } },
+        { $project: { _id: 1, title: 1, description: 1 } },
+      ])
+      .exec();
+
+    return simplifiedArts;
+  }
 }
